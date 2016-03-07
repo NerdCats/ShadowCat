c3e1bc42f27418d695d87abf29126ab8fd6b1a95
Mahmud <mahmud@gobd.co>
2016-03-07 16:23:01 +0600

    Add configuration file, not working yet

---
+++ device_api/config.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+
+# Server configurations
+DEBUG = True
+SERVER_NAME = '0.0.0.0:7000'
+JSON_AS_ASCII = False
+
+# Database configurations
+DB_HOST = 'taskcatmongo.cloudapp.net'
+DB_PORT = 27017
+DB_CLIENT = MongoClient(DB_HOST, DB_PORT)
+DB_NAME = 'shadowcat'
+DB_DATABASE = DB_CLIENT[DB_NAME]
+
+COLL_HTTP_DEVICES = 'httpDevices'
+COLL_PINGS = 'pings'
+COLL_HISTORY = 'history'
+
+DB_COLL_DEVICES = DB_DATABASE.COLL_HTTP_DEVICES
+DB_COLL_PINGS = DB_DATABASE.COLL_PINGS
+DB_COLL_HISTORY = DB_DATABASE.COLL_HISTORY
--- device_api/dapi.py
+++ device_api/dapi.py
@@ -1,24 +1,24 @@
 from flask import Flask, request
-from pymongo import MongoClient
+# from pymongo import MongoClient
 from bson.json_util import dumps
 from datetime import datetime, timedelta
 from models import User
 
+# host = 'taskcatmongo.cloudapp.net'
+# port = 27017
+# client = MongoClient(host, port)
+# db = client.shadowcat                           # Database: shadowcat
+# coll_devices = db.httpDevices                   # Collection: httpDevice
+# coll_current_ping = db.currentLocation          # Collection: currentLocation
+# coll_location_history = db.locationHistory      # Collection: locationHistory
+
+# print client, '\n'
+# print db, '\n'
+# print coll_location_history, '\n'
+# print coll_current_ping, '\n'
+# print coll_devices, '\n'
+
 app = Flask(__name__)
-
-host = 'taskcatmongo.cloudapp.net'
-port = 27017
-client = MongoClient(host, port)
-db = client.shadowcat                           # Database: shadowcat
-coll_devices = db.httpDevices                   # Collection: httpDevice
-coll_current_ping = db.currentLocation          # Collection: currentLocation
-coll_location_history = db.locationHistory      # Collection: locationHistory
-
-print client, '\n'
-print db, '\n'
-print coll_location_history, '\n'
-print coll_current_ping, '\n'
-print coll_devices, '\n'
 
 
 @app.route('/')
@@ -35,7 +35,7 @@ def register_device():
         json_data["point"],
         json_data["device"]
     )
-    coll_devices.insert_one(data.__dict__)
+    app.config['DB_COLL_DEVICES'].insert_one(data.__dict__)
     return dumps(data.__dict__), 201
 
 
@@ -48,16 +48,16 @@ def ping_location():
         json_data["name"],
         json_data["point"]
     )
-    coll_location_history.insert_one(data.__dict__)
+    app.config['DB_COLL_HISTORY'].insert_one(data.__dict__)
 
-    user_data = coll_current_ping.find_one({"user_id": json_data["user_id"]})
+    user_data = app.config['DB_COLL_PINGS'].find_one({"user_id": json_data["user_id"]})
     if not user_data:
-        coll_current_ping.insert_one(data.__dict__)
+        app.config['DB_COLL_PINGS'].insert_one(data.__dict__)
     else:
-        coll_current_ping.update_one({"user_id": json_data["user_id"]},
-                                     {'$set': {"point": json_data["point"],
-                                               "timestamp": datetime.utcnow() + timedelta(hours=6)}
-                                      })
+        app.config['DB_COLL_PINGS'].update_one({"user_id": json_data["user_id"]},
+                                               {'$set': {"point": json_data["point"],
+                                                         "timestamp": datetime.utcnow() + timedelta(hours=6)}
+                                                })
     # return jsonify({'data': data}), 201
     return dumps(data.__dict__), 201
 
@@ -66,7 +66,7 @@ def ping_location():
 @app.route('/api/current', methods=['POST', 'GET'])
 def ping_current():
     if request.method == 'GET':
-        cursor = coll_current_ping.find()
+        cursor = app.config['DB_COLL_PINGS'].find()
         current_locations = []
         for document in cursor:
             current_locations.append(document)
@@ -82,19 +82,19 @@ def ping_current():
             json_data["point"]
         )
 
-        user_data = coll_current_ping.find_one({"user_id": json_data["user_id"]})
+        user_data = app.config['DB_COLL_PINGS'].find_one({"user_id": json_data["user_id"]})
         if not user_data:
-            coll_current_ping.insert_one(data)
+            app.config['DB_COLL_PINGS'].insert_one(data)
         else:
-            coll_current_ping.update_one({"user_id": json_data["user_id"]},
-                                         {'$set': {"point": json_data["point"]}})
+            app.config['DB_COLL_PINGS'].update_one({"user_id": json_data["user_id"]},
+                                                   {'$set': {"point": json_data["point"]}})
 
         return dumps(data), 201
 
 
 @app.route('/api/devices', methods=['GET'])
 def get_devices():
-    cursor = coll_devices.find()
+    cursor = app.config['DB_COLL_DEVICES'].find()
     device_list = []
     for document in cursor:
         device_list.append(document)
@@ -102,20 +102,6 @@ def get_devices():
     return dumps(device_list)
 
 
-@app.route('/api/config', methods=['GET'])
-def get_config():
-    data = {
-        'client': client,
-        'database': db,
-        'coll_devices': coll_devices,
-        'coll_current_ping': coll_current_ping,
-        'coll_location_history': coll_location_history
-    }
-    return dumps(data)
-
-
 if __name__ == "__main__":
-    app.run(
-        host='0.0.0.0',
-        debug=True
-    )
+    app.config.from_object('config')
+    app.run()
