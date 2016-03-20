d876d841b494d051120e0f6f4e5592bba6911c2d
Mahmud <mahmud@gobd.co>
2016-03-20 10:49:36 +0600

    Refactor load_config.py
    Load configuration file safely using 'with' statement so that file is
    automatically closed when not needed.

--- VehicleTrackerDemo/load_config.py
+++ VehicleTrackerDemo/load_config.py
@@ -2,28 +2,28 @@ import json
 
 
 def get_configurations(config_file):
-    json_data = open(config_file).read()
-    data = json.loads(json_data)
+    with open(config_file) as json_data:
+        data = json.load(json_data)
 
-    if "host" in data:
-        host = data["host"]
-    else:
-        host = "127.0.0.1"
+        if "host" in data:
+            host = data["host"]
+        else:
+            host = "127.0.0.1"
 
-    if "port" in data:
-        port = data["port"]
-    else:
-        port = 32767
+        if "port" in data:
+            port = data["port"]
+        else:
+            port = 32767
 
-    if "buffer_size" in data:
-        buffer_size = data["buffer_size"]
-    else:
-        buffer_size = 1024
+        if "buffer_size" in data:
+            buffer_size = data["buffer_size"]
+        else:
+            buffer_size = 1024
 
-    if "server_queue" in data:
-        queue_size = data["server_queue"]
-    else:
-        queue_size = 5
+        if "server_queue" in data:
+            queue_size = data["server_queue"]
+        else:
+            queue_size = 5
 
     server_config = {
         "host": host,
